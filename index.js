12d0b312bbf5f4e344baad58240107ecf3356a24
Divine Smith <deraia264@gmail.com>
2025-03-14 16:19:29 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,30 @@
+document.write("<h1>Divine's form :)</h1>")
+
+// var d=new Date();
+// var time=d.getHours();
+// if (time<10)
+// {
+// document.write("<b>Good morning</b>");
+// }
+// else{
+//     document.write("<b>Good day!</b>");
+// }
+// alert(d);
+
+// var d = newDate();
+// var time = d.getTime();
+// if (time < 10)
+// {
+//     document.write("<b>Good morning</b>")
+// }
+// alert(d.getTime() + " " + d.alertText)
+// if (age < 16)
+//     document.write("Too young")
+
+// if (age = 17) 
+//     document.write("old enough")
+
+// if (age = 18) 
+//     document.write("just right")
+// greeting=(visitor=="PRES")?"Dear President ":"Dear ";
+// console.log("greeting")
